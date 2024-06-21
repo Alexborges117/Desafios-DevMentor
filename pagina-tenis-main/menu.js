f0f1cd6757e8c92b2f283a62351c7a5aecce68f5
Alex Borges <97403345+Alexborges117@users.noreply.github.com>
2024-06-21 16:45:18 -0300

    At1

--- pagina-tenis-main/menu.js
+++ pagina-tenis-main/menu.js
@@ -1,14 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const hamburguer = document.querySelector(".hamburguer");
-    const menu = document.querySelector("ul.menu");
-    const compra = document.querySelector(".Compra");
-    const body = document.body;
-  
-    hamburguer.addEventListener("click", function() {
-      menu.classList.toggle("show");
-      compra.classList.toggle("show");
-      hamburguer.classList.toggle("open");
-      body.classList.toggle("no-scroll");
-    });
+  const hamburguer = document.querySelector(".hamburguer");
+  const menu = document.querySelector("ul.menu");
+  const compra = document.querySelector(".Compra");
+  const body = document.body;
+  const twoContent = document.querySelector(".twoContent");
+
+  hamburguer.addEventListener("click", function() {
+    menu.classList.toggle("show");
+    compra.classList.toggle("show");
+    hamburguer.classList.toggle("open");
+    body.classList.toggle("no-scroll");
+
+    // Toggle da classe 'hidden' no .twoContent
+    twoContent.classList.toggle("hidden");
   });
-  
+});
